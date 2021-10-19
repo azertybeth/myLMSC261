0fcfad6e2d432e21ea8690cb13d948fc313a3236
azertybeth <90657388+azertybeth@users.noreply.github.com>
2021-10-18 20:03:20 -0400

    In-class excercise

---
+++ Control Flow/testv1.py
@@ -0,0 +1,16 @@
+def seasons():
+    month = int(input("Enter a number between 1 and 12: "))
+    if 9 >= month >= 7:
+	    print("damn girl is it just global warming or is it summer")
+    if 6 >= month >= 4:
+	    print("spring time babeeeeey")
+    if 11 >= month >= 10:
+	    print("it's called autumn not fall")
+    if 3 >= month:
+	    print("brrrrrrrrr")
+    if month == 12:
+      print("brrrrrrrrr")
+    if month > 12:
+      print("there are only twelve months mate")
+
+seasons()
---
+++ Control Flow/testv2.py
@@ -0,0 +1,13 @@
+def seasons():
+    month = int(input("Enter a number between 1 and 12: "))
+    if 9 >= month >= 7:
+	    print("damn girl is it just global warming or is it summer")
+    elif 6 >= month >= 4:
+	    print("spring time babeeeeey")
+    elif 11 >= month >= 10:
+	    print("it's called autumn not fall")
+    elif 3 >= month or 12 == month:
+	    print("brrrrrrrrr")
+    else:
+      print("there are only twelve months mate")
+seasons()
